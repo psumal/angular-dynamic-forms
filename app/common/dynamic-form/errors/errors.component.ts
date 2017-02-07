11f3788649dbbdd1033b26c78eddbb3d0407f410
Michael Hladky <michael@hladky.at>
2017-02-07 22:38:06 +0100

    fix(error comp): edit

--- app/common/dynamic-form/errors/errors.component.ts
+++ app/common/dynamic-form/errors/errors.component.ts
@@ -1,15 +1,56 @@
 // my-component.component.ts
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ItemBase} from "../item/item-base";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
-  selector: 'item-error',
-  template: `
-    <div class="item-error">
-      <ng-content></ng-content>
-    </div>
-  `
+  selector: 'item-errors',
+  templateUrl:'./errors.component.html'
 })
-export class ItemErrorComponent {
+export class ItemErrorComponent implements OnInit{
+
+  get errors(): {} {
+    return this._errors;
+  }
+
+  set errors(errors: {}) {
+    errors = errors || {};
+    this._errors = this._getMessagesByErrors(errors);
+  }
+
+  @Input() item: ItemBase<any> = <any>{};
+  @Input() form: FormGroup = <any>{};
+
+  private _errors:{};
+
+  errorMessageMap = {
+    required:"This value is required!"
+  };
+
+  ngOnInit() {
+    this.errors = this.form.get(this.item.key).errors;
+
+    let $statusChanges = this.form.get(this.item.key).statusChanges;
+    $statusChanges.subscribe((status) => {
+      this.errors = this.form.get(this.item.key).errors;
+    });
+  }
+
+  errorKeys() : Array<string> {
+    return Object.keys(this.errors) || [];
+  }
+
+  getClassNames() {
+    return "form-control-feedback";
+  }
+
+  _getMessagesByErrors(errors:{}){
+    let mappedErrors = {};
+    for (let key of Object.keys(errors)) {
+      mappedErrors[key] = this.errorMessageMap[key] || 'No message given for error name '+key;
+    }
+    return mappedErrors;
+  }
 
 }
